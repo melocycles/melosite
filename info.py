ad3a3862e7114222309d3bac2ebb80cfc0d79211
guillaumek <dg.kaba@gmail.com>
2024-03-03 22:52:47 +0100

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 import os
 
-USER_UUID=os.environ['USER_UUID']
-ADMIN_UUID=os.environ['ADMIN_UUID']
-APP_SECRET = os.environ['APP_SECRET']
+USER_UUID=os.environ.get('USER_UUID', '')
+ADMIN_UUID=os.environ.get('ADMIN_UUID', '')
+APP_SECRET = os.environ.get('APP_SECRET', '')
