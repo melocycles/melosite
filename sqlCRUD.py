b8840ce39f677c5216ad49a9e52edf776c138c4d
melocycles <louisromieu@protonmail.com>
2024-03-12 13:28:55 +0100

    toujours test

--- sqlCRUD.py
+++ sqlCRUD.py
@@ -268,7 +268,7 @@ def readBike(whoCall : str, dictOfFilters : dict = None) -> list[dict]:
     cursor = connection.cursor()
     cursor.execute(sqlQuerry) # éxécute la requette
     result = cursor.fetchall()
-    print(result)
+    print("sqlCRUD ligne 271 : ", result)
     connection.close()
     rows = []
 
