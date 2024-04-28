5eca3617d79e270848ad49b9032295c0306383c1
melocycles <louisromieu@protonmail.com>
2024-04-28 20:28:49 +0200

    .

--- info.py
+++ info.py
@@ -5,5 +5,4 @@ USER_UUID = os.environ.get('USER_UUID', '')
 ADMIN_UUID = os.environ.get('ADMIN_UUID', '')
 APP_SECRET = os.environ.get('APP_SECRET', '')
 
-with open(os.environ.get("herokuJson", ''), 'r', encoding="UTF8") as f:
-    JSONCONFIG = json.load(f)
+JSONCONFIG = json.load(os.environ.get("herokuJson", ''))
