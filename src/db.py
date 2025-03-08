04a689176e8099e901f1f427ecb0fdc9b74d80f9
truite-codeuse <troutdev@protonmail.com>
2025-03-08 22:36:19 +0100

    Update db.py comments and typing
    Added warning comment at the top of the file, added a type return to the get_db() function.

--- src/db.py
+++ src/db.py
@@ -1,5 +1,6 @@
 from flask import g
 import psycopg2
+from __future__ import annotations
 
 #  WARN: This maybe not a great implementation and will only work in localhost
 # TODO: Check if this code can get a db conn on heroku
@@ -7,37 +8,19 @@ import psycopg2
 
 class Database():
     """
-        Class used to get the connection to the database
+        Class used to get the connection to the database.
+        This class is not instiable, you can get the db connection using Database.get_db()
     """
 
-    def __init__(self, host: str,
-                 db_name: str,
-                 user: str,
-                 password: str,
-                 is_localhost=False):
-        """Setup the data for a postgres db connection.
-
-        Keyword arguments:
-        host -- name of the host (ex: "localhost")
-        db_name -- name of the database (ex: "melodb")
-        user -- user that will launch database (ex: "postgres")
-        password -- password for the user (ex: nice try hehe)
-        is_localhost -- is the app running in localhost or on server (default False)
-        """
-        self._host = host
-        self._db_name = db_name
-        self._user = user
-        self._password = password
-        self._is_localhost = is_localhost
-
-    def get_db(self) -> psycopg2.connection:
+    @classmethod
+    def get_db(self) -> psycopg2.connect:
         """Returns the database connection, if it does not exist it creates it."""
         if "db" not in g:
             g.db = psycopg2.connect(
-                host=self._host,
-                database=self._db_name,
-                user=self._user,
-                password=self._password
+                host="localhost",
+                database="melodb",
+                user="postgres",
+                password="mdp"
             )
         # This is a typical implementation of a singleton design pattern
         # There is and will exist only ONE connection to the database in one app run
